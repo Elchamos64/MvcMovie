7bc6ccaf8bde1702d96066ee243bc8e679e7b515
Oscar Ramos <ram22018@byui.edu>
2024-11-14 20:04:36 -0700

    Add project files.

---
+++ Migrations/20241115024004_AddFilePath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFilePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Movie");
+        }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "The R.M.",
+                    ReleaseDate = DateTime.Parse("2003-1-31"),
+                    Genre = "Comedy",
+                    Price = 7.99M,
+                    Image = "~/images/the-rm.jpg"
+                },
+                new Movie
+                {
+                    Title = "The Other Side of Heaven ",
+                    ReleaseDate = DateTime.Parse("2001-12-14"),
+                    Genre = "Adventure",
+                    Price = 8.99M,
+                    Image = "~/images/the-other-side-of-heaven.jpg"
+                },
+                new Movie
+                {
+                    Title = "The fightingh Preacher",
+                    ReleaseDate = DateTime.Parse("2019-7-24"),
+                    Genre = "Comedy",
+                    Price = 9.99M,
+                    Image = "~/images/the-fighting-preacher.jpg"
+                },
+                new Movie
+                {
+                    Title = "Meet the Mormons",
+                    ReleaseDate = DateTime.Parse("2014-10-10"),
+                    Genre = "Documentary",
+                    Price = 3.99M,
+                    Image = "~/images/meet-the-mormons.jpg"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
